718c70b4e3203099edc74da28675f17a78b4fa2f
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-24 19:48:05 -0300

    Added SignUp - Dashboard (CSS and JS)

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css'
 
 function App() {
   return (
@@ -38,7 +39,77 @@ function App() {
       </aside>
 
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/30740305?s=460&u=0edb35254172e05ca713bd3862aa983d34e9320d&v=4" alt="claudio pereira" />
 
+              <div className="user-info">
+                <strong>Claudio Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>CTO da @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</p>
+            <a href="https://github.com/claudiofrancopereira">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/30740305?s=460&u=0edb35254172e05ca713bd3862aa983d34e9320d&v=4" alt="claudio pereira" />
+
+              <div className="user-info">
+                <strong>Claudio Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>CTO da @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</p>
+            <a href="https://github.com/claudiofrancopereira">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/30740305?s=460&u=0edb35254172e05ca713bd3862aa983d34e9320d&v=4" alt="claudio pereira" />
+
+              <div className="user-info">
+                <strong>Claudio Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>CTO da @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</p>
+            <a href="https://github.com/claudiofrancopereira">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/30740305?s=460&u=0edb35254172e05ca713bd3862aa983d34e9320d&v=4" alt="claudio pereira" />
+
+              <div className="user-info">
+                <strong>Claudio Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>CTO da @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</p>
+            <a href="https://github.com/claudiofrancopereira">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/30740305?s=460&u=0edb35254172e05ca713bd3862aa983d34e9320d&v=4" alt="claudio pereira" />
+
+              <div className="user-info">
+                <strong>Claudio Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>CTO da @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</p>
+            <a href="https://github.com/claudiofrancopereira">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
