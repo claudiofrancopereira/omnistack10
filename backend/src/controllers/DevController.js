628ecf9154ef310b7836dda2d3d488a2e7053175
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-24 02:26:19 -0300

    Added Search using Lat/Long and Techs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
     async index(request, response) {
@@ -20,7 +21,7 @@ module.exports = {
     
             console.log(name, avatar_url, bio, github_username)
     
-            const techsArray = techs.split(',').map(tech => tech.trim());
+            const techsArray = parseStringAsArray(techs);
     
             const location = {
                 type: 'Point',
