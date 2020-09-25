cfcd1b14260b12fda523f7284a119081f7cc9907
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-25 03:25:10 -0300

    Added Search Form

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -33,21 +34,37 @@ function Main({ navigation }) {
         return null;
     }
 
-    return ( 
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -21.126698, longitude: -48.972925 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/737884?s=460&v=4' }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'claudiofrancopereira' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Claudio Pereira</Text>
-                        <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, Node.js </Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+    return (
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -21.126698, longitude: -48.972925 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/737884?s=460&v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'claudiofrancopereira' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Claudio Pereira</Text>
+                            <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, Node.js </Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -85,6 +102,44 @@ const styles = StyleSheet.create({
     devTechs: {
         marginTop: 5,
 
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 
 });
