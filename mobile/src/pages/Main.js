eeaea496bbcd656a9fdd6b9e6100b75ea90ffdbf
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-25 03:01:57 -0300

    Added Markers on the map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -33,13 +33,58 @@ function Main() {
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map} />
+    return ( 
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -21.126698, longitude: -48.972925 }}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/737884?s=460&v=4' }} />
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Claudio Pereira</Text>
+                        <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</Text>
+                        <Text style={styles.devTechs}>ReactJS, React Native, Node.js </Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+
     },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+
+    },
+
+    devTechs: {
+        marginTop: 5,
+
+    }
+
 });
 
 export default Main;
