1525eaae8c13e28bfe2e23328222b4d11b950b07
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-25 03:12:47 -0300

    Added webview to App

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,7 +3,7 @@ import { StyleSheet, Image, View, Text } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -37,7 +37,9 @@ function Main() {
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -21.126698, longitude: -48.972925 }}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/737884?s=460&v=4' }} />
-                <Callout>
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', { github_username: 'claudiofrancopereira' })
+                }}>
                     <View style={styles.callout}>
                         <Text style={styles.devName}>Claudio Pereira</Text>
                         <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile</Text>
